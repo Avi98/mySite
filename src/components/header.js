d8dc719aedbf3707cde62e4cc0afd85f8f8bd710
webwerks1 <webwerks1@XYZ-123.local>
2018-10-09 18:07:33 +0530

    feat(Header): header background added

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 
 import {HeaderImg} from '../image'
+import {FlexContainer} from './commonStyles';
+
+
 const HeaderBg = styled.div`
-background-color: coral;
+    height: 15rem;
+    background-image: url(${HeaderImg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 `;
+const HeaderTitle = styled(FlexContainer)`
+  display: flex;
+  color: aliceblue;
+  padding: 10vh 0;
+`
+
+
 const Header = () => {
   return ( 
-    <div>
-   <HeaderBg>
-    sdd
-
-   </HeaderBg>
-    </div>
+    <HeaderBg>
+      <HeaderTitle>
+        <h1>
+          Avinash
+        </h1>
+        <h2>
+          React developer
+        </h2>
+      </HeaderTitle>
+    </HeaderBg>
    );
 }
  
